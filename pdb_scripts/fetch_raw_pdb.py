3d1c666677efeccd9fa154c75a129fa3bf601589
awakenwu <373419356@qq.com>
2019-05-27 13:51:57 +0800

    Initial commit

---
+++ pdb_scripts/fetch_raw_pdb.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from sys import argv
+from os import system,path
+import os
+import ConfigParser
+
+labdatabases = os.environ["PDB_DIR"]
+
+# remote host for downloading pdbs
+#remote_host = os.environ["INET_HOST"]
+
+pdbname = argv[1]
+
+netpdbname = labdatabases
+netpdbhost = ""
+#if remote_host:
+#    netpdbhost = remote_host
+
+if not netpdbname.endswith(path.sep):
+    netpdbname += path.sep
+
+netpdbname += pdbname[1:3] + '/' + pdbname
+
+url = 'https://files.rcsb.org/download/%s.pdb' % (pdbname.lower())
+wget_cmd = 'wget -c -t 0 %s' % (url)
+os.system(wget_cmd)
+
+#if netpdbhost:
+#    system("scp %s:%s.pdb ." % ( netpdbhost, netpdbname) )
+#else:
+#    system("cp %s.pdb ." % netpdbname )
